cdc0f359fc45a9fced9f0e01f371686247de55e2
Gabriel Souza <souzagabrielpacheco@gmail.com>
2025-03-11 17:19:33 -0300

    first commit

---
+++ PrimeiroProjeto/src/br/com/souza/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.souza;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!\n");
+	}
+
+}
